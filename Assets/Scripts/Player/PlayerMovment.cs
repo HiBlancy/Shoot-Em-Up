d706c6a459c59f763de0c497e488ce46d70e31e5
HiBlancy <ievarituma9877@gmail.com>
2022-12-08 21:05:23 +0100

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovment.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float moveInputX = Input.GetAxis("Horizontal");
+        float moveInputY = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(moveInputX * speed, moveInputY * speed);
+    }
+}
